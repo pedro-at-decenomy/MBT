5229595bc622830ce13cf22ecec58e0dbbdd9f14
perseu <jmsaguas@gmail.com>
2021-02-02 18:24:14 +0100

    Adding copyright info, second search parameters.

--- src/legacy/validation_zerocoin_legacy.h
+++ src/legacy/validation_zerocoin_legacy.h
@@ -1,4 +1,5 @@
 // Copyright (c) 2020 The PIVX developers
+// Copyright (c) 2021 The Mobolith developers
 // Distributed under the MIT/X11 software license, see the accompanying
 // file COPYING or https://www.opensource.org/licenses/mit-license.php.
 
